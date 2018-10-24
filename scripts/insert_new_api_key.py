ff91ffa71093140c1b619181b5566510542a42d3
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-10-24 11:48:10 +0100

    New API keys are hashed

--- scripts/insert_new_api_key.py
+++ scripts/insert_new_api_key.py
@@ -1,6 +1,7 @@
 import rethinkdb
 import random
 import time
+import hashlib
 
 conn = rethinkdb.connect( "localhost", 28015).repl()
 
@@ -8,10 +9,11 @@ username = input("username: ")
 print("Generate new key")
 full_wallet_seed = hex(random.SystemRandom().getrandbits(128))
 api_key = full_wallet_seed[2:].upper()
+api_key_hashed = hashlib.sha512(k.encode('utf-8')).hexdigest()
 
-print("%s %s" % (username, api_key))
+print("%s %s %s" % (username, api_key, api_key_hashed))
 
 print("Insert into DB")
-data = rethinkdb.db("pow").table("api_keys").insert({"username":username, "api_key":api_key, "count": 0,  "time":time.time()}).run()
+data = rethinkdb.db("pow").table("api_keys").insert({"username":username, "api_key":api_key_hashed, "count": 0,  "time":time.time()}).run()
 print(data)
 print("complete")
