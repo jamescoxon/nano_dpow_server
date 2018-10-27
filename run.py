5cef5e0935ae9041c5e39e2a71fa728e843b9809
Guilherme Lawless <guilherme.lawless@tecnico.ulisboa.pt>
2018-10-27 10:30:39 +0100

    Dont colide with nano python API

--- run.py
+++ run.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import nano
+import nanoutils
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
